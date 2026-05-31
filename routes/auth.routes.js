2d338204117a5be9b90c3ec08168b90b8b67ab49
shadon016 <shadon016@gmail.com>
2026-05-31 09:55:38 +0600

    fix

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,17 +1,12 @@
 import { Router } from "express";
-import {
-  register,
-  login,
-  refresh,
-  logout,
-} from "../controllers/auth.controller.js";
+import { login, refresh, logout } from "../controllers/auth.controller.js";
 import { validate } from "../middlewares/validate.middleware.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 import { registerSchema, loginSchema } from "../validations/auth.validation.js";
 
 const router = Router();
 
-router.post("/register", validate(registerSchema), register);
+// router.post("/register", validate(registerSchema), register);
 router.post("/login", validate(loginSchema), login);
 router.post("/refresh", refresh);
 router.post("/logout", authMiddleware, logout);
